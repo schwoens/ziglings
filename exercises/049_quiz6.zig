b69130665b35a8edc1adbbd3d6af298e404cc745
schwoens <schwoens@protonmail.com>
2023-09-21 09:11:32 +0200

    complete exercises 37-50

--- exercises/049_quiz6.zig
+++ exercises/049_quiz6.zig
@@ -27,7 +27,13 @@ const Elephant = struct {
     // Your Elephant trunk methods go here!
     // ---------------------------------------------------
 
-    ???
+    pub fn getTrunk(self: *const Elephant) *Elephant {
+        return self.trunk.?;
+    }
+
+    pub fn hasTrunk(self: *const Elephant) bool {
+        return (self.trunk != null);
+    }
 
     // ---------------------------------------------------
 
